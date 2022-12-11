530d3a30337a3591100625828a6f038e61ca19c7
Alex <collagememorycollage@gmail.com>
2022-12-11 13:49:42 +0300

    сократили все до одного презентора

--- src/main.js
+++ src/main.js
@@ -3,11 +3,9 @@ import BoardPresenter from './presenter/board-presenter';
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
 const siteFooterElement = document.querySelector('.footer');
-const mainPresenter = new BoardPresenter({main: siteMainElement});
-const headerPresenter = new BoardPresenter({header: siteHeaderElement});
-const footerPresenter = new BoardPresenter({footer: siteFooterElement});
+const mainPresenter = new BoardPresenter({header: siteHeaderElement, main: siteMainElement, footer: siteFooterElement});
 
 // headerPresenter.initHerader();
-headerPresenter.initHeader();
+mainPresenter.initHeader();
 mainPresenter.initMain();
-footerPresenter.initFooter();
+mainPresenter.initFooter();
