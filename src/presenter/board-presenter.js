fb0b51356341b11a22536791a7bb363b9a215419
Alex <collagememorycollage@gmail.com>
2022-12-25 14:45:23 +0300

    добавили отрисовку для добавления новых комментов

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -33,14 +33,6 @@ export default class BoardPresenter {
   #sectionFilmsComponent = new SectionFilmsView();
 
 
-  #userNameStatusComponent = new UserNameStatusView();
-  #popupFilmSectionView = new PopupFilmSectionView();
-  #popupFilmCommentList = new PopupFilmCommentListView();
-  #popupFilmFeatilsInnerView = new PopupFilmDetailsInnerView();
-  #popupFilmDetailsBottomContainerView = new PopupFilmDetailsBottomContainerView();
-  #popupFilmDetailsCommentsWrapView = new PopupFilmDetailsCommentsWrapView();
-  #popupFilmDetailNewCommentView = new PopupFilmDetailNewCommentView();
-
   #filmListComponent = new FilmListView();
   #filmContainer = this.#filmListComponent.element.querySelector('.films-list__container');
 
@@ -88,6 +80,8 @@ export default class BoardPresenter {
     for(let i = 0; i < movie.movie.comments.length; i++){
       render(new PopupFilmCommentStructureView(this.#loadedComments[i]) ,commentList);
     }
+    render(new PopupFilmDetailNewCommentView(), commentList);
+
 
     const closePopup = () => {
       popupView.element.parentElement.removeChild(popupView.element);
@@ -127,67 +121,6 @@ export default class BoardPresenter {
 
 
   }
-  // initHeader() {
-  //   render(this.#userNameStatusComponent, this.#header);
-  // }
-
-
-  // initMain() {
-  //   render(new MenuView(), this.#main);
-  //   render(new SortView(), this.#main);
-  //   render(this.#filmListComponent, this.#main);
-  //   render(this.#sectionFilmsComponent, this.#filmListComponent.element);
-  //   render(new FilmListTitleView(), this.#sectionFilmsComponent.element);
-  //   render(this.#filmListContainerComponent, this.#sectionFilmsComponent.element);
-  //   this.#listMovie = [...this.#movieModel.movie];
-  //   for (let i = 0; i < this.#listMovie.length; i++) {
-  //     this.#renderMovieCards(this.#listMovie[i]);
-  //   }
-  //   render(new ShowMoreButtonView(), this.#sectionFilmsComponent.element);
-  //   render(new SectionFilmListExtraView(), this.#sectionFilmsComponent.element);
-  //   render(new SectionFilmListExtraView(), this.#sectionFilmsComponent.element);
-  // }
-
-  // initFooter() {
-  //   render(new FooterStatisticsView(), this.#footer);
-  // }
-
-  // initPopup() {
-
-  //   render(this.#popupFilmSectionView, this.#body);
-  //   render(this.#popupFilmFeatilsInnerView, this.#popupFilmSectionView.element);
-  //   this.#renderMoviePopup(this.#popupMovie);
-  //   render(this.#popupFilmDetailsBottomContainerView, this.#popupFilmFeatilsInnerView.element);
-  //   render(this.#popupFilmDetailNewCommentView, this.#popupFilmDetailsCommentsWrapView.element);
-  // }
-
-
-  // #renderMovieCards(movie) {
-  //   const cardComponent = new CardFilmsView({movie});
-  //   render(cardComponent, this.#filmListContainerComponent.element);
-  //   cardComponent.element.querySelector('.film-card__link').addEventListener('click', ()=>{
-  //     this.initPopup();
-  //     this.#renderMoviePopup(movie);
-  //     this.#body.classList.add('hide-verflow');
-  //   });
-  // }
-
-  // #renderMoviePopup(movie) {
-  //   const popupComponent = new PopupView(movie);
-  //   const popupCommentsComponent = new PopupFilmDetailsCommentsTitleView(movie);
-  //   render(popupComponent, this.#popupFilmFeatilsInnerView.element);
-  //   render(this.#popupFilmDetailsCommentsWrapView, this.#popupFilmDetailsBottomContainerView.element);
-  //   render (popupCommentsComponent, this.#popupFilmDetailsCommentsWrapView.element);
-  //   render(this.#popupFilmCommentList,this.#popupFilmDetailsCommentsWrapView.element);
-  //   for (let i = 0; i < this.#popupMovie.comments.length; i++) {
-  //     render(new PopupFilmCommentStructureView(this.#popupMovie.comments[i]), this.#popupFilmCommentList.element);
-  //   }
-  //   popupComponent.element.querySelector('.film-details__close-btn').addEventListener('click', ()=>{
-  //     const popupElement = document.querySelector('.film-details');
-  //     popupElement.remove();
-  //     this.#body.classList.remove('hide-overflow');
-  //   });
-  // }
 }
 
 
