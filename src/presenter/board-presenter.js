afd48a97aa4b0f0da1ca014ba3cc8caa5596c9f8
Alex <collagememorycollage@gmail.com>
2022-12-25 15:00:39 +0300

    добавил отрисовку комментариев при помощи forEach

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -3,19 +3,11 @@ import FilmListView from '../view/film-list-view.js';
 import SectionFilmsView from '../view/section-films-view.js';
 import MenuView from '../view/menu-view.js';
 import { render } from '../render.js';
-
 import SortView from '../view/sort-view.js';
 import ShowMoreButtonView from '../view/show-more-button-view.js';
-
-import SectionFilmListExtraView from '../view/section-film-list-extra-view.js';
 import UserNameStatusView from '../view/user-name-status-view.js';
 import FooterStatisticsView from '../view/footer-statistics-view.js';
 import PopupView from '../view/popup-view';
-import PopupFilmSectionView from '../view/popup-film-section-view.js';
-import PopupFilmCommentListView from '../view/popup-film-comment-list-view.js';
-import PopupFilmDetailsInnerView from '../view/popup-film-details-inner-view.js';
-import PopupFilmDetailsBottomContainerView from '../view/popup-film-details-bottom-container-view.js';
-import PopupFilmDetailsCommentsWrapView from '../view/popup-film-details-comments-wrap-view.js';
 import PopupFilmDetailsCommentsTitleView from '../view/popup-film-details-comments-title-view.js';
 import PopupFilmCommentStructureView from '../view/popup-film-comment-structure-view.js';
 import PopupFilmDetailNewCommentView from '../view/popup-film-details-new-comment-view.js';
@@ -77,9 +69,13 @@ export default class BoardPresenter {
     const filmDetailsCommentsTitle = popupView.element.querySelector('.film-details__comments-title');
     render(new PopupFilmDetailsCommentsTitleView(movie),filmDetailsCommentsTitle);
     const commentList = popupView.element.querySelector('.film-details__comments-list');
-    for(let i = 0; i < movie.movie.comments.length; i++){
-      render(new PopupFilmCommentStructureView(this.#loadedComments[i]) ,commentList);
-    }
+    // for(let i = 0; i < movie.movie.comments.length; i++){
+    //   render(new PopupFilmCommentStructureView(this.#loadedComments[i]) ,commentList);
+    // }
+    movie.movie.comments.forEach((element, index) => {
+      render(new PopupFilmCommentStructureView(this.#loadedComments[index]) ,commentList);
+    });
+
     render(new PopupFilmDetailNewCommentView(), commentList);
 
 
