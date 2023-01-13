bacc6276695ec634549c177e68e760ffce17c76d
Alex <collagememorycollage@gmail.com>
2023-01-13 11:12:03 +0300

    добавили во вью фильтров логику"

--- src/view/menu-view.js
+++ src/view/menu-view.js
@@ -1,20 +1,20 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
-function createMenuTemplate({name, count}) {
+function createMenuTemplate({name, count}, isSelected) {
   return `
-  <a href="#${name}" class="main-navigation__item">${name} <span class="main-navigation__item-count">${count}</span></a>
+  <a href="#${name}" class="main-navigation__item ${isSelected ? 'main-navigation__item--active' : ''} ${count === 0 ? 'main-navigation__item' : ''}">${name} <span class="main-navigation__item-count">${count}</span></a>
 `;
 }
 
 
 function createFilterTemplate(filterItems) {
   const filterItemsTemplate = filterItems
-    .map((filter) => createMenuTemplate(filter))
+    .map((filter, index) => createMenuTemplate(filter, index === 0))
     .join('');
 
   return (
     `<nav class="main-navigation">
-    <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
+    <a href="#all" class="main-navigation__item">All movies</a>
       ${filterItemsTemplate}
         </nav>`
   );
