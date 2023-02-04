3336f91bab4bc03457185fd42dd989964f9ad473
Alex <collagememorycollage@gmail.com>
2023-02-05 02:11:09 +0300

    закончили с первой частью задания

--- src/model/movies-model.js
+++ src/model/movies-model.js
@@ -9,9 +9,29 @@ export default class MovieModel extends Observable {
     return this.#movie;
   }
 
+  set movie(movie) {
+    this.#movie = movie;
+  }
+
   get comments() {
     return this.#comments;
   }
 
+
+  updateMovie(updatedType, update) {
+    const index = this.#movie.findIndex((movie) => movie.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t update unexisting film');
+    }
+
+    this.#movie = [
+      ...this.#movie.slice(0, index),
+      update,
+      ...this.#movie.slice(index + 1)
+    ];
+
+    this._notify(updatedType, update);
+  }
 }
 
